5e99e289af657f6e51d79f0163b8c9183ac07051
23Rishiraj <rishirajsinghbhl2003@gmail.com>
2025-08-06 23:15:37 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://chatalyst.onrender.com", // Update with your frontend URL
+        origin:["http://localhost:3000", "https://chatalyst.onrender.com","http://localhost:5000"], // Update with your frontend URL
         methods: ["GET", "POST"],
         // credentials: true,
     },
