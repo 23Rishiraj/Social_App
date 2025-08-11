8a337d2f264cf2405b496803c0752e75932ca709
23Rishiraj <rishirajsinghbhl2003@gmail.com>
2025-08-11 11:13:01 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) =>{
     const [onlineUsers, setOnlineUsers] = useState([]);
 
     useEffect(()=>{
-        const socket = io("http://localhost:5000", {
+        const socket = io("https://chatalyst-backend.onrender.com", {
             query:{
                 userId: user?._id,
                 // username: user.username,
